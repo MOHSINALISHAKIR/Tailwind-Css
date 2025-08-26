990bd4ebc25e97ef193f11eb1fc8fb8629501e77
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-26 15:02:29 +0500

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
+import Grid from './Grid'
 
 const App = () => {
   return (
     <>
-    <h1 className='text-2xl text-center font-bold'>Our Service</h1>
+    {/* <h1 className='text-2xl text-center font-bold'>Our Service</h1>
     <div className='grid grid-cols-3 gap-4 max-w-[900px] mx-auto mt-5'>
         <div className='rounded-lg border'>
-            <img className='h-[350px] hover:scale-95'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, beatae.</p>
             <h4>Enroll Now</h4>
         </div>
         <div>
-            <img className='h-[350px] hover:scale-95'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, facere.</p>
             <h4>Enroll Now</h4>
         </div>
         <div className='rounded-lg border'>
-            <img className='h-[350px] hover:scale-95'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, beatae.</p>
             <h4>Enroll Now</h4>
         </div>
         <div>
-            <img className='h-[350px] hover:scale-95'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, facere.</p>
             <h4>Enroll Now</h4>
         </div>
         <div className='rounded-lg border'>
-            <img className='h-[350px] hover:scale-95'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://images.unsplash.com/photo-1755396884135-de0d28d460dd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, beatae.</p>
             <h4>Enroll Now</h4>
         </div>
         <div>
-            <img className='h-[350px] hover:scale-95'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='h-[350px] hover:scale-95 w-[100%]'  src="https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, facere.</p>
             <h4>Enroll Now</h4>
         </div>
 
-    </div>
+    </div> */}
+    <Grid/>
     </>
   )
 }
---
+++ src/Grid.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <div className='w-[1200px] mx-auto'>
+        <h1 className='text-2xl text-green-600'>Grid Layout</h1>
+        <div className='grid sm:grid-cols-1 md:grid-cols-3 grid-cols-4 gap-2'>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://plus.unsplash.com/premium_photo-1755534520242-75aaf271b790?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+            <div className='border w-[400px] p-2 rounded'>
+                <img src="https://images.unsplash.com/photo-1755845711249-32cfcdcabfeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" className='w-[400px] h-[250px]' alt="" />
+                <h4 className='text-[18px] font-bold'>Lorem ipsum dolor sit amet.</h4>
+                <p className='text-gray-600 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolore?</p>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md text-center'>See More</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Grid
